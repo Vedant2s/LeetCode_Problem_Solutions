ee850f1f1dd257bd0779581aa9e8c63062513f67
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2024-01-07 10:42:36 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (38.94%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int u = 0; // Use a single index to track the position to overwrite
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != val) {
-                nums[u] = nums[i];
+        vector<int> nums2;
+        int u=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums2.push_back(nums[i]);
                 u++;
-            }
+            }     
         }
-        
+        for(int i=0;i<u;i++){
+            nums.at(i)=(nums2.at(i));
+        }
         return u;
     }
 };
